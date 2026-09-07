4b65ac3b6570e1afd642c956cf9d84cd90610d90
Mr. Dexter <137222384+mr-dexter-ssj@users.noreply.github.com>
2026-09-07 17:24:12 -0500

    chore: remove commented symlink creation for sounds
    Removed symlink creation for sounds directory.

--- globalquake/globalquake.sh
+++ globalquake/globalquake.sh
@@ -4,7 +4,5 @@
 GCDATA="${XDG_DATA_HOME:-$HOME/.local/share}/globalquake"
 mkdir -p "$GCDATA"
 cd "$GCDATA"
-# It also needs a symlink for sounds
-#ln -sfn /usr/share/globalquake/sounds sounds
 
 java -Xmx1G -cp /usr/share/java/globalquake/GlobalQuake.jar:/usr/share/java/globalquake/libs/* net.globalquake.client.main.Main
